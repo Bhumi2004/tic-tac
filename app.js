4fba8257e67e3522c7b2a75dce3486752f76577c
Bhumi2004 <115335044+Bhumi2004@users.noreply.github.com>
2024-02-17 21:51:20 +0530

    added all files

---
+++ app.js
@@ -0,0 +1,110 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn =document.querySelector("#reset");
+let setbutton=document.querySelector("#set");
+let msgcontainer=document.querySelector(".msg-container")
+let msgpara = document.querySelector("#msg");
+
+let turnX = true;//palyerX palyerY
+let count=0;
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+boxes.forEach((box)=>
+{
+    box.addEventListener("click",()=>
+    {
+        console.log("box was clicked");
+        if(turnX)
+        {
+        box.innerText="X";
+        turnX=false;
+        }
+        else{
+            box.innerText="O";
+            turnX=true;
+        }
+        box.disabled=true;
+        count++;
+        let isWinner=checkWinner();
+        if(count===9 && !isWinner)
+        {
+            noWinner();
+        }
+
+        
+    });
+});
+const enableboxes=()=>{
+    for(box of boxes)
+    {
+        box.disabled=false;
+        box.innerText="";
+        
+
+    }
+}
+
+const disableboxes=()=>{
+    for(box of boxes)
+    {
+        box.disabled=true;
+       
+    }
+}
+
+const showWinner=(winner)=>{
+
+   msg.innerText=`Congratulations,Winner is ${winner} :)`;
+   msgcontainer.classList.remove("hide");
+   disableboxes();
+
+};
+const checkWinner=()=>
+{
+    for(let pattern of winPatterns)
+    {
+            let pos1val=   boxes[pattern[0]].innerText;
+            let pos2val=   boxes[pattern[1]].innerText;
+            let pos3val=   boxes[pattern[2]].innerText;
+
+            if(pos1val !="" && pos2val !="" && pos3val!="")
+            {
+                if(pos1val===pos2val && pos2val===pos3val)
+                {
+                    
+                    showWinner(pos1val);
+                    
+                }
+            }
+    }
+};
+
+
+const setGame=()=>{
+     turnX = true;
+     count=0;
+     enableboxes();
+     msgcontainer.classList.add("hide");
+    
+};
+
+const noWinner=()=>
+{
+    msg.innerText=`Opps It Is a Draw!:(`;
+    msgcontainer.classList.remove("hide");
+    disableboxes();
+}
+   
+resetbtn.addEventListener("click",setGame);
+setbutton.addEventListener("click",setGame);
+
+
+
